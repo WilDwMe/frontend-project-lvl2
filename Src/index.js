f51bbae8c1b616301c61f53b7e0b62c97a857b3a
WilD <a.nazarenko@nylonfabric.ru>
2021-11-14 15:35:46 +0300

    add console logs

--- Src/index.js
+++ Src/index.js
@@ -13,18 +13,24 @@ const def = (file1, file2) => {
   for (const key in sortedOne) {
     if (_.has(sortedTwo, key)) {
       if (sortedOne[key] === sortedTwo[key]) {
+        // continue;
         result[key] = fileOne[key]; 
+        console.log(`${key} : ${fileOne[key]}`);
       } else if (sortedOne[key] !== sortedTwo[key]) {
         result[key] = sortedOne[key];
         result[key] = sortedTwo[key];
+        console.log(`- ${key} : ${sortedOne[key]}`);
+        console.log(`+ ${key} : ${sortedTwo[key]}`);
       }
     } else if (_.has(sortedOne, key)) {
       result[key] = sortedOne[key];
+      console.log(`- ${key} : ${sortedOne[key]}`);
     }
   }
   for (const key in sortedTwo) {
     if (!_.has(sortedOne, key)) {
       result[key] = sortedTwo[key];
+      console.log(`+ ${key} : ${sortedTwo[key]}`);
     }
   }
   return result;
