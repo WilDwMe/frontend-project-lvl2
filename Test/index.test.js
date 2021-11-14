2995ee4c7a036929ab226ef34e83e2726868aa2a
WilD <a.nazarenko@nylonfabric.ru>
2021-11-14 14:26:12 +0300

    fix linter warnings

--- Test/index.test.js
+++ Test/index.test.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '../', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+// const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('GenDiff - main function', () => {
   const file1 = getFixturePath('file1.json');
