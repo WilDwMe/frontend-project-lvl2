4dd912ff29fea7f823ffe2cec938513cf0a541b3
WilDwMe <wild.msk@gmail.com>
2021-11-15 17:00:42 +0300

    checkpoint

--- Src/modules/parsers.js
+++ Src/modules/parsers.js
@@ -3,16 +3,15 @@ import yaml from 'js-yaml';
 import path from 'path';
 
 export const parser = (data) => {
-    const parseJson = (data) => JSON.parse(fs.readFileSync(data));
-    const parseYml = (data) => yaml.load(fs.readFileSync(data));
 
     const format = path.extname(data);
  
   if (format === '.json') {
+    const parseJson = (data) => JSON.parse(fs.readFileSync(data));
     return parseJson(data);
   } else if (format === '.yml') {
-    return parseJson(data);
+    const parseYml = (data) => yaml.load(fs.readFileSync(data));
+    return parseYml(data);
   }
-
 
 }
--- Test/index.test.js
+++ Test/index.test.js
@@ -21,10 +21,9 @@ test('GenDiff - main function', () => {
   });
 });
 
-const file3 = getFixturePath('file3.json');
-const file4 = getFixturePath('file4.json');
-
 test('GenDiff - other case', () => {
+  const file3 = getFixturePath('file3.json');
+  const file4 = getFixturePath('file4.json');
   expect(def(file3, file4)).toEqual({
     age: 40,
     master: true,
@@ -34,10 +33,9 @@ test('GenDiff - other case', () => {
   });
 });
 
-const file5 = getFixturePath('file5.yml');
-const file6 = getFixturePath('file6.yml');
-
 test('GenDiff - Yaml case', () => {
+  const file5 = getFixturePath('file5.yml');
+  const file6 = getFixturePath('file6.yml');
   expect(def(file5, file6)).toEqual({
     follow: false,
     host: 'hexlet.io',
@@ -47,3 +45,53 @@ test('GenDiff - Yaml case', () => {
     verbose: true,
   });
 });
+
+// test('GenDiff - Deep equal case', () => {
+//   const file1 = getFixturePath('fileDeep1.json');
+//   const file2 = getFixturePath('fileDeep2.json');
+//   expect(def(file1, file2)).toEqual({
+//     common: {
+//       follow: false,
+//       setting1: 'Value 1',
+//       setting2: 200,
+//       setting3: true,
+//       // setting3: null,
+//       setting4: 'blah blah',
+//       setting5: {
+//         key5: 'value5',
+//       },
+//       setting6: {
+//         doge: {
+//           // wow: ,
+//           wow: 'so much',
+//         },
+//         key: 'value',
+//         ops: 'vops',
+//       },
+//     },
+//     group1: {
+//       // baz: 'bas',
+//       baz: 'bars',
+//       foo: 'bar',
+//       // nest: {
+//       //   key: 'value',
+//       // },
+//       nest: 'str',
+//     },
+//     group2: {
+//       abc: 12345,
+//       deep: {
+//         id: 45,
+//       },
+//     },
+//     group3: {
+//       deep: {
+//         id: {
+//           number: 45,
+//         },
+//       },
+//       fee: 100500,
+//     },
+//   });
+// });
+// 
